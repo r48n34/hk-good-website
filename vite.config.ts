372facd64d6cff9e6b508881e1b4d0307c373a09
reemo <r48n34@gmail.com>
2024-02-24 20:28:09 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import Sitemap from 'vite-plugin-sitemap'
 
 import react from '@vitejs/plugin-react'
+import { categoryList } from "./src/data/tempData"
+
+const dynamicRoutes = categoryList.map(v => `/category/${v}`)
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        Sitemap()
+        Sitemap({
+            dynamicRoutes,
+            hostname: "https://hk-good-website.vercel.app/"
+        })
     ],
 })
